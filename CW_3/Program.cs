7e8cacb15611bf661094c30367e742afcf29ec40
Richard Hargis <ricfharg@ut.utm.edu>
2020-08-29 11:44:15 -0500

    number guessing game

--- CW_3/Program.cs
+++ CW_3/Program.cs
@@ -7,6 +7,41 @@ namespace CW_3
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            string menuChoice;
+            Random rnd = new Random();
+            
+            int num = rnd.Next(1, 100);
+            Console.WriteLine("I'm thinking of a number between 1-100\n");
+            //Console.WriteLine(num);
+            string numGuess;
+            Console.WriteLine("Guess the number: ");
+            numGuess = Console.ReadLine();
+            int res = Convert.ToInt32(numGuess);
+            if(res < num)
+            {
+                Console.WriteLine("Guess a higher number");
+            }
+            else if(res > num)
+            {
+                Console.WriteLine("Guess a lower number");
+            }
+            while(res != num)
+            {
+                Console.WriteLine("Guess the number: ");
+                numGuess = Console.ReadLine();
+                res = Convert.ToInt32(numGuess);
+                Console.WriteLine("Try again...\n");
+                //Console.WriteLine(num);
+                if (res < num)
+                {
+                    Console.WriteLine("Guess a higher number");
+                }
+                else if (res > num)
+                {
+                    Console.WriteLine("Guess a lower number");
+                }              
+            }
+
         }
     }
 }
